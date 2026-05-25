54f233718f6a918c578e61cf932dde6a019f635b
mmr4tn@umsystem.edu <mmr4tn@umsystem.edu>
2026-05-24 21:11:20 -0500

    added to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 curl -fsSL https://ollama.com/install.sh | sh
 git clone https://github.com/ggml-org/whisper.cpp.git
+sh ./models/download-ggml-model.sh tiny.en
 sudo apt-get install libsdl2-dev
 cmake -B build -DWHISPER_SDL2=ON
 cmake --build build -j --config Release
